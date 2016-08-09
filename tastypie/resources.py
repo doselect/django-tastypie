01b0078cb22a3285c04731a833b1985adff8d20b
Parth Oberoi <parth@doselect.com>
2016-08-09 17:16:27 +0530

    Fixing kwargs extraction in get_via_uri
    - uses find instead of rfind
    
    Signed-off-by: Parth Oberoi <parth@doselect.com>

--- tastypie/resources.py
+++ tastypie/resources.py
@@ -833,7 +833,7 @@ class Resource(six.with_metaclass(DeclarativeMetaclass)):
         # We mangle the path a bit further & run URL resolution against *only*
         # the current class. This ought to prevent bad URLs from resolving to
         # incorrect data.
-        found_at = chomped_uri.rfind(self._meta.resource_name)
+        found_at = chomped_uri.find(self._meta.resource_name)
         if found_at == -1:
             raise NotFound("An incorrect URL was provided '%s' for the '%s' resource." % (uri, self.__class__.__name__))
         chomped_uri = chomped_uri[found_at:]
